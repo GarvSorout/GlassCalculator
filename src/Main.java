98bec472d158e9e37144429bce24f91f3e1b65f7
GarvSorout <gsorout04@gmail.com>
2023-06-19 18:40:32 -0700

    updated for all user input variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the first number");
+        double firstNumber = scanner.nextDouble();
+        System.out.println("Enter the operation");
+        char operation = scanner.next().charAt(0);
+        System.out.println("Enter the second number");
+        double secondNumber = scanner.nextDouble();
+
     }
 }
+
