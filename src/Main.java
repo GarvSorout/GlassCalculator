c123dc43cc7b6701f5b1466f6784abebf1e82c4e
GarvSorout <gsorout04@gmail.com>
2023-06-19 19:08:04 -0700

    lots of problems, basic skeleton for the calculator

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,41 @@ public class Main {
         char operation = scanner.next().charAt(0);
         System.out.println("Enter the second number");
         double secondNumber = scanner.nextDouble();
+        output test = new output(0.0);
+        return test.calculator(firstNumber, operation, secondNumber);
 
     }
 }
 
+class output {
+    double result;
+    //constructor for output class
+    public output(Double result){
+        this.result = result;
+    }
+    //method to run the calculation
+    public double calculator(double firstNumber, char operation, double secondNumber){
+        switch (operation){
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+            case '/':
+                if (secondNumber != 0){
+                    result = firstNumber / secondNumber;
+                }
+                else {System.out.println("division by zero is not allowed");
+                }
+                break;
+            default: System.out.println("Invalid operator");
+
+
+
+        }
+       return result;
+    }
+}
